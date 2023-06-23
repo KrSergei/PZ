acb1f9ce8574f912c0d8077fc3505004c539aadb
KrSergei <34679066+KrSergei@users.noreply.github.com>
2023-06-24 00:32:29 +0300

    Add bullet movement

--- PZ/Assets/Scripts/Bullet/BulletController.cs
+++ PZ/Assets/Scripts/Bullet/BulletController.cs
@@ -4,18 +4,24 @@ public class BulletController : MonoBehaviour
 {    
     public float speed;
 
+    public Rigidbody2D rb;
     [SerializeField]
     private float lifeTime;
+    [SerializeField]
+    private bool _isPhisicsBody;
 
     public float LifeTime { get => lifeTime;  private set => lifeTime = value; }
 
-    void Start()
+    private void Start()
     {
-       
+        rb = GetComponent<Rigidbody2D>();
+        _isPhisicsBody = rb ? true : false;
     }
+
 
     private void FixedUpdate()
     {
-      
+        if (_isPhisicsBody) rb.AddForce(Vector2.right * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        else transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
     }
 }
