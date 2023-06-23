e4370c32bed7a153a731579336d30247839b0cff
KrSergei <34679066+KrSergei@users.noreply.github.com>
2023-06-24 00:36:49 +0300

    Refactoring

--- PZ/Assets/Scripts/Bullet/BulletController.cs
+++ PZ/Assets/Scripts/Bullet/BulletController.cs
@@ -1,27 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class BulletController : MonoBehaviour
 {    
     public float speed;
-
     public Rigidbody2D rb;
+
     [SerializeField]
     private float lifeTime;
-    [SerializeField]
-    private bool _isPhisicsBody;
 
     public float LifeTime { get => lifeTime;  private set => lifeTime = value; }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        _isPhisicsBody = rb ? true : false;
     }
 
 
     private void FixedUpdate()
     {
-        if (_isPhisicsBody) rb.AddForce(Vector2.right * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        else transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
+        rb.AddForce(Vector2.right * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);       
     }
 }
