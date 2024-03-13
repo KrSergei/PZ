52250520e72569c025c20870085e665d4defe80b
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-03-13 20:22:19 +0300

    added animations

--- PZ/Assets/Scripts/UI/UIItem.cs
+++ PZ/Assets/Scripts/UI/UIItem.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,6 +7,7 @@ public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHa
     private CanvasGroup _canvasGroup;
     [SerializeField] private Canvas _mainCanvas;
     private RectTransform _rectTransform;
+    [SerializeField] private bool _isUsingItem = false;
 
     private void Start()
     {
@@ -19,6 +21,8 @@ public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHa
         var slotTransform = _rectTransform.parent;
         slotTransform.SetAsLastSibling();
         _canvasGroup.blocksRaycasts = false;
+        _isUsingItem = true;
+        UsingItemPeriod();
     }
     public void OnDrag(PointerEventData eventData)
     {
@@ -28,7 +32,14 @@ public class UIItem : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHa
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (_isUsingItem) Debug.Log("Using item");
         transform.localPosition = Vector3.zero;
         _canvasGroup.blocksRaycasts = true;
     }
+
+    async private void UsingItemPeriod() {
+        await UniTask.Delay(200);
+        _isUsingItem = false;
+    }
+
 }
